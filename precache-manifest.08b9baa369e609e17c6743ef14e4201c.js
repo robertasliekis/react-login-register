d4730874e77f489b71754a5f1f2a3bf8cf5ac1a5
Robertas Liekis <robertasliekis@gmail.com>
2020-09-30 13:01:59 +0300

    Updates

---
+++ precache-manifest.08b9baa369e609e17c6743ef14e4201c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccea97a7c08a332ad4d9bbafc0f4d47b",
+    "url": "//robertasliekis.github.io/react-login-register/index.html"
+  },
+  {
+    "revision": "91c3dd13b9f9c416f94f",
+    "url": "//robertasliekis.github.io/react-login-register/static/css/main.44ae84d1.chunk.css"
+  },
+  {
+    "revision": "4b9c02365cfcf9981ead",
+    "url": "//robertasliekis.github.io/react-login-register/static/js/2.d653fdbd.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "//robertasliekis.github.io/react-login-register/static/js/2.d653fdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c3dd13b9f9c416f94f",
+    "url": "//robertasliekis.github.io/react-login-register/static/js/main.d7903159.chunk.js"
+  },
+  {
+    "revision": "59628a65c0fc97027ba6",
+    "url": "//robertasliekis.github.io/react-login-register/static/js/runtime-main.065fe9b2.js"
+  }
+]);
