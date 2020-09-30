db4cc0eb6703a6e36af7f78b9f193d6bcbadff84
Robertas Liekis <robertasliekis@gmail.com>
2020-09-30 13:15:12 +0300

    Updates

---
+++ precache-manifest.cbc57d5a8372d9436dc8dbd4fad20cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd0c8d5fb749f99be11b909133aa8c6",
+    "url": "/react-login-register/index.html"
+  },
+  {
+    "revision": "1611a9562871f238db5c",
+    "url": "/react-login-register/static/css/main.44ae84d1.chunk.css"
+  },
+  {
+    "revision": "4b9c02365cfcf9981ead",
+    "url": "/react-login-register/static/js/2.d653fdbd.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-login-register/static/js/2.d653fdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1611a9562871f238db5c",
+    "url": "/react-login-register/static/js/main.d2522160.chunk.js"
+  },
+  {
+    "revision": "ab487f52713bcffc5fdf",
+    "url": "/react-login-register/static/js/runtime-main.af93f4b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//robertasliekis.github.io/react-login-register/precache-manifest.49e95b392d11eb4870da4a8577f3f990.js"
+  "/react-login-register/precache-manifest.cbc57d5a8372d9436dc8dbd4fad20cd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//robertasliekis.github.io/react-login-register/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-login-register/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
