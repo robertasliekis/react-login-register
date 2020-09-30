b157c1113e030f8788b3abdc6411e6c6a431c7af
Robertas Liekis <robertasliekis@gmail.com>
2020-09-30 13:07:49 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+
+import { connect } from "react-redux";
+
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import About from "./components/About";
+import UserInfo from "./components/UserInfo";
+import EditUserInfo from "./components/EditUserInfo";
+import WrongPage from "./components/WrongPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="website-wrapper">
+            <Header />
+            <div className="main-content">
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/user" component={UserInfo} />
+                <Route path="/edit" component={EditUserInfo} />
+                <Route path="/about" component={About} />
+                <Route path="*" component={WrongPage} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    loginStatus: state.isLoggedIn.isLoggedIn,
+  };
+};
+
+export default connect(mapStateToProps)(App);
